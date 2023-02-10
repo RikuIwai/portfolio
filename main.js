442009f3fa5a5e49ba2bb9c4a061319e68658421
riku iwai <iwariku0410@gmail.com>
2023-02-10 15:35:46 +0900

    新規ポートフォリオ

---
+++ main.js
@@ -0,0 +1,122 @@
+// ハンバーガー
+const ham = $("#js-hamburger");
+const nav = $("#js-nav");
+
+ham.on("click", function () {
+  ham.toggleClass("active");
+  nav.toggleClass("active");
+});
+
+// スキルページ
+$(function () {
+  $(".skill_explain").hover(
+    function () {
+      $(".skill_explain-text").fadeIn();
+    },
+    function () {
+      $(".skill_explain-text").fadeOut();
+    }
+  );
+});
+
+// ローディングスピナー
+window.onload = function () {
+  const loader = document.querySelector(".m_load");
+  loader.classList.add("m_load-after");
+
+  const content = document.querySelector(".l_content-visible");
+  content.style.visibility = "visible";
+};
+
+gsap.fromTo(
+  ".top_kv__title",
+  {
+    duration: "1",
+    autoAlpha: "0",
+    x: "-500",
+  },
+  {
+    x: "0",
+    autoAlpha: "1",
+    duration: "3",
+  }
+);
+
+gsap.from(".js_top_about-profile", {
+  duration: "0.8",
+  autoAlpha: "0",
+  x: "-500",
+  scrollTrigger: {
+    trigger: ".top_title",
+    start: "top center",
+  },
+  stagger: {
+    each: "0.3",
+    from: "end",
+  },
+});
+gsap.from(".js_top_about-career-detail", {
+  duration: "1",
+  autoAlpha: "0",
+  y: "200",
+  scrollTrigger: {
+    trigger: ".top_about-tit",
+    start: "top top",
+  },
+  stagger: {
+    each: "0.6",
+    from: "start",
+  },
+});
+
+let skillExplain = document.querySelectorAll(".skill_explain-text");
+gsap.from(skillExplain, {
+  duration: 2,
+  autoAlpha: 0,
+  scrollTrigger: {
+    trigger: "skillExplain.parentNode",
+  },
+  stagger: {
+    each: 1,
+    from: "start",
+  },
+});
+
+// 実績ページのモーダル
+
+$(function () {
+  let jsWorks = $(".js_works");
+  let jsModal = $(".js_modal");
+
+  jsWorks.on("click", function () {
+    $(this).children(jsModal).toggleClass("is-active");
+  });
+});
+
+// アコーディオン
+const faq = document.querySelectorAll(".js_faq");
+
+faq.forEach((element) => {
+  const faqA = element.querySelector(".js_faq-a");
+  const mark = element.querySelector(".js_faq_mark");
+
+  element.addEventListener("click", () => {
+    if (element.classList.contains("is-active")) {
+      // ①アコーディオンを閉じるときの処理を記述してください
+      // ②アイコン操作用クラスを切り替える(クラスを取り除く)処理を記述してください
+      // ③GSAPで作成したアニメーションを実行する関数を記述してくだい
+      // ①の処理
+      faqA.classList.toggle("is-active");
+      //②の処理
+      mark.classList.toggle("is-open");
+    } else {
+      // ①アコーディオンを開くときの処理を記述してください
+      // ②アイコン操作用クラスを切り替える(クラスを付与)処理を記述してください
+      //③GSAPで作成したアニメーションを実行する関数を記述してください
+      // ①の処理
+      faqA.classList.toggle("is-active");
+      // ②の処理
+      mark.classList.toggle("is-open");
+    }
+  });
+});
